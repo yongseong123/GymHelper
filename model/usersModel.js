7420fe2b72637177426817a53a84022e794e0a81
ys763 <yammy5701@gmail.com>
2024-10-04 21:50:42 +0900

    회원가입 기능 테스트 확인

--- model/usersModel.js
+++ model/usersModel.js
@@ -4,21 +4,21 @@ const { poolPromise } = require("./index");
 exports.addNewUser = async function (userInfo) {
   const { id, hashedPassword, username } = userInfo;
   const pool = await poolPromise;
-  await pool.query`INSERT INTO user(id, password, username) VALUES
+  await pool.query`INSERT INTO users(id, password, username) VALUES
                       (${id}, ${hashedPassword}, ${username});`;
 };
 
 exports.getUserById = async function (id) {
   const pool = await poolPromise;
   const { recordset } =
-    await pool.query`SELECT * FROM user WHERE id = ${id}`;
+    await pool.query`SELECT * FROM users WHERE id = ${id}`;
   return recordset;
 };
 
 exports.checkIdDuplication = async function (id) {
   const pool = await poolPromise;
   const { recordset } =
-    await pool.query`SELECT id FROM user WHERE id = ${id}`;
+    await pool.query`SELECT id FROM users WHERE id = ${id}`;
 
   if (recordset.length > 0) return true;
   return false;
@@ -26,22 +26,22 @@ exports.checkIdDuplication = async function (id) {
 
 exports.changePassword = async function (id, newPassword) {
   const pool = await poolPromise;
-  await pool.query`UPDATE user SET password = ${newPassword} WHERE id = ${id}`;
+  await pool.query`UPDATE users SET password = ${newPassword} WHERE id = ${id}`;
 };
 
 exports.changeName = async function (id, newName) {
   const pool = await poolPromise;
-  await pool.query`UPDATE user SET username = ${newName} WHERE id = ${id}`;
+  await pool.query`UPDATE users SET username = ${newName} WHERE id = ${id}`;
 }
 
 exports.getUserName = async function (id) {
   const pool = await poolPromise;
   const { recordset } =
-    await pool.query`SELECT username FROM user WHERE id = ${id}`;
+    await pool.query`SELECT username FROM users WHERE id = ${id}`;
   return recordset[0]?.name;
 };
 
 exports.deleteUser = async function (id) {
   const pool = await poolPromise;
-  await pool.query`DELETE FROM user WHERE id = ${id}`;
+  await pool.query`DELETE FROM users WHERE id = ${id}`;
 }
